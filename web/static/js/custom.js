b35811c4b5d147634eca9d42a036992989cf4f7c
Brian Schaper <briansc@gmail.com>
2018-03-17 01:11:40 -0700

    Added flask and static site

---
+++ web/static/js/custom.js
@@ -0,0 +1,12 @@
+
+
+$('#soundcapture').click(function() {
+  console.log('#soundcapture was clicked');
+    $.getJSON($SCRIPT_ROOT + '/_toggle_soundcapture', {
+  }, function(data) {
+    $("#soundcapture").text(function(i, text){
+        return text === "On" ? "Off" : "On";
+    })
+  });
+  return false;
+});
