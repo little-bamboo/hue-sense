e3aac54ca71cfa22a6a0ca76304e14fca8b3c162
Brian Schaper <briansc@gmail.com>
2018-03-26 23:35:26 -0700

    added hue buttons and help page routes

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,3 +1,25 @@
+$('#light_switch').click(function () {
+    var toggle_state = document.getElementById('light_switch').innerText;
+    $.getJSON($SCRIPT_ROOT + '/hue/_toggle_lights/' + toggle_state, {}, function (data) {
+        console.log(data);
+        $("#light_switch").text(function (i, text) {
+            console.log('text: ' + text);
+            var button = document.getElementById('light_switch')
+
+            if (text === 'On') {
+                button.classList.remove('btn-success');
+                button.classList.add('btn-danger');
+            } else {
+                button.classList.add('btn-success');
+                button.classList.remove('btn-danger');
+            }
+
+            return text === "On" ? "Off" : "On";
+        })
+    });
+    return false;
+});
+
 $('#soundcapture').click(function () {
     var toggle_state = document.getElementById('soundcapture').innerText;
     $.getJSON($SCRIPT_ROOT + '/hue/_toggle_soundcapture/' + toggle_state, {}, function (data) {
@@ -21,11 +43,5 @@ $('#soundcapture').click(function () {
 });
 
 $(document).ready(function () {
-    $('.nav a').click(function () {
-        //removing the previous selected menu state
-        $('.nav').find('li.active').removeClass('active');
-        //adding the state for this parent menu
-        $(this).parents("li").addClass('active');
-
-    });
-});
+    $("#{{request.endpoint}}").parent().addClass("active");
+})
